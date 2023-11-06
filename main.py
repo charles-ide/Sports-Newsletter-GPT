312723c33abb967586c633cc9e951e5525f6c3f5
charles-ide <charles.adams.ide@gmail.com>
2023-11-06 13:03:15 -0500

    Adding initial two files:

---
+++ main.py
@@ -0,0 +1,6 @@
+'''
+(c) Charles Ide, November 6 2023
+This file will serve as the entrypoint for our Newsletter GPT application
+'''
+
+from web_interface import 
---
+++ web_interface.py
@@ -0,0 +1,3 @@
+# (c) Charles Ide, November 6 2023
+# This file will be used to host a webpage where users can interact with my application
+
