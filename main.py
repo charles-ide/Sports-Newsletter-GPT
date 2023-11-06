8cb2d2b0bcc315aaf1645223a333bfbfc24234ee
charles-ide <charles.adams.ide@gmail.com>
2023-11-06 13:49:16 -0500

    For real done

--- main.py
+++ main.py
@@ -3,4 +3,23 @@
 This file will serve as the entrypoint for our Newsletter GPT application
 '''
 
-from web_interface import 
+#from web_interface import index, process_url
+from flask import Flask, render_template, request
+from url_processor import get_page_content
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process_url', methods=['POST'])
+def process_url_route():
+    if request.method == 'POST':
+        url = request.form['url']
+        page_content = get_page_content(url)
+        return render_template('result.html', url=url, page_content=page_content)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
